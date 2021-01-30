8f669c2a09d4adb84828a4aed7356051f7a5cecc
joelkoz <joel.kozikowski@gmail.com>
2021-01-29 19:07:55 -0500

    Fixed deprecated Parser floating point method
    binary-parser-encoder changed method name of floating point fields.

--- DBDepthLog.js
+++ DBDepthLog.js
@@ -6,11 +6,11 @@ class DBDepthLog extends CircularFile {
     // Depth log consists of the last 30 days worth of data (assuming data reads every 5 minutes)
     constructor(recordDataInterval) {
         super(Parser.start()
-                    .double("timer")
-                    .float("depth")
+                    .doublebe("timer")
+                    .floatbe("depth")
                     .nest("pos", { type: Parser.start()
-                                                .double("latitude")
-                                                .double("longitude")
+                                                .doublebe("latitude")
+                                                .doublebe("longitude")
                         }),
                         30 * 24 * (60 / recordDataInterval));
     }
